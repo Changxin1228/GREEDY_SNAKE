fc9c6f0010823ea4bac7c56c80d75d00bafa68bb
刘光衡 <122103571+changxin1228@users.noreply.github.com>
2023-05-07 08:50:23 +0800

    注释 更新版

--- Gre_Sna.c
+++ Gre_Sna.c
@@ -1,13 +1,14 @@
-#pragma warning (disable:4996)
+#pragma warning (disable:4996)//禁用掉一些警告
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
 #include<time.h>
 #include<stdlib.h>
+//定义所有游戏中的名词转会为数值
 #define ROW 22//行
 #define COL 42//列
 #define WALL 1//墙
-#define KONG 0//空
+#define KONG 0//空白
 #define HEAD 77//蛇头
 #define BODY 88//蛇身
 #define FOOD 99//食物
@@ -17,28 +18,30 @@
 #define RIGHT 77 //右
 #define SPACE 32 //暂停
 #define ESC 27 //退出
-struct snake_head
+struct snake_head//定义蛇的头部和身长
 {
-	int len;
+	int len;//身长
+	//蛇在游戏空空间中 头的坐标
 	int x;
 	int y;
 }snake;
-struct snake_body
+struct snake_body//定义
 {
+	//某一蛇身坐标 x，y
 	int x;
 	int y;
-}body[ROW*COL];
+}body[ROW*COL];//定一个一足够记录所有蛇身体的坐标二维数组
 
-int face[ROW][COL];
+int face[ROW][COL];//记录下游戏界面所有位置的信息
 
-int max, grade;
+int max, grade;//最高分，和高得分记录
 
 
 
 //函数定义
-void color(int c)//颜色
+void ColorForInterface(int c)//设置当前界面颜色
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c); //颜色设置
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c); //颜色设置，获取控制台句柄
 }
 void CursorJump(int x ,int y)//光标跳转
 {
@@ -47,14 +50,13 @@ void CursorJump(int x ,int y)//光标跳转
 	pos.Y = y;
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE); 
 	SetConsoleCursorPosition(handle, pos);
+	//SetConsoleCursorPosition是光标跳转函数通过获取控制台句柄以及结构体COORD中的x，y坐标确定光标位置
 }
 char GameBegin()//游戏初始界面
 {
 	printf("************************************************************************************************************************\n");
-	//system("color 70");
-	//printf("\t\t\t\t\t\tGREEDY_SNAKE\n");
 	printf("\n");
-	color(4);
+	ColorForInterface(4);//红色的greedy snake
 	printf("\t\t\t\t ■■ ■  ■■■■■   ■■■■■■ ■■■■■■ ■■■■   ■■■ ■■■\n");
 	printf("\t\t\t\t■  ■■   ■   ■   ■      ■      ■  ■   ■   ■ \n");
 	printf("\t\t\t\t■   ■   ■   ■   ■      ■      ■   ■  ■   ■ \n");
@@ -77,20 +79,26 @@ char GameBegin()//游戏初始界面
 	char a;
 	printf("\n");
 	printf("\n\n\n\n");
-	color(7);
+	ColorForInterface(7);
 	printf("\t\t\t\t■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	
 	printf("\t\t\t\t\t\t开始游戏(y/n):");
 	scanf("%c", &a);
+	getchar();//吃掉回车
+	if (a=='n'||a=='N')
+	{
+		system("cls");
+		printf("\t\t\t欢迎下次再来");
+		exit(-1);
+	}
 	return a;
 }
 void HideCursor()//光标隐藏
 {
-	CONSOLE_CURSOR_INFO cursor;
+	CONSOLE_CURSOR_INFO cursor;//定义光标结构体CONSOLE_CURSOR_INFO
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	cursor.dwSize = 1;
 	cursor.bVisible = FALSE;
-	SetConsoleCursorInfo(handle, &cursor);
+	SetConsoleCursorInfo(handle, &cursor);//光标隐藏函数，同样需要获取句柄
 }
 void ReadMaxGrade()//读取最高成绩到max
 {
@@ -100,12 +108,13 @@ void ReadMaxGrade()//读取最高成绩到max
 		fp = fopen("greedy_snake.txt", "w");
 		fwrite(&max, sizeof(int), 1, fp);
 	}
-	fseek(fp, 0, SEEK_SET);
-	fread(&max, sizeof(int), 1, fp);
+	//打开或者创建文件
+	fseek(fp, 0, SEEK_SET);//光标定位到文件的开头
+	fread(&max, sizeof(int), 1, fp);//读取一个max
 	fclose(fp);
 	fp = NULL;
 }
-void WriteGrade()//更新最高分
+void UpDateGread()//更新最高分
 {
 	FILE* fp = fopen("greedy_snake.txt", "w");
 	if (fp == NULL)
@@ -113,22 +122,22 @@ void WriteGrade()//更新最高分
 		printf("找不到指定文件");
 		exit(-1);
 	}
-	fwrite(&grade, sizeof(int), 1, fp);
+	fwrite(&grade, sizeof(int), 1, fp);//格式化写入
 	fclose(fp);
 	fp = NULL;
 }
-void InitInterface()
+void InitInterface()//初始化游戏界面 包括墙 空白 
 {
-	color(4);//墙设置为红色
+	ColorForInterface(4);//墙设置为红色
 	int i, j;
-	for (i = 0; i < ROW; i++)
+	for (i = 0; i < ROW; i++)//打印墙，选取行列的最大值进行打印
 	{
 		for (j = 0; j < COL;j++)
 		{
 			if (j == 0 || j == COL - 1)
 			{
-				face[i][j] = WALL;
-				CursorJump(2 * j, i);
+				face[i][j] = WALL;//将此位置记录成墙
+				CursorJump(2 * j, i);//光标跳转，2*j扩大游戏版面
 				printf("■ ");
 			}
 			else if (i == 0 || i == ROW - 1)
@@ -138,27 +147,28 @@ void InitInterface()
 			}
 			else
 			{
-				face[i][j] = KONG;
+				face[i][j] = KONG;//其他的位置暂时设置为空白位置
 			}
 		}
 	}
-	color(7); //颜色设置为白色
+	ColorForInterface(7); //颜色设置为白色
+	//在游戏下方打印得分和最高得分
 	CursorJump(0, ROW);
 	printf("当前得分:%d", grade);
 	CursorJump(COL, ROW);
 	printf("历史最高得分:%d", max);
 }
-void InitSnake()//初始化蛇
+void InitSnake()//对蛇的初始化，包括蛇的初始位置，蛇位置的记录
 {
-	snake.len = 2;
-	snake.x = COL / 2;
+	snake.len = 2;//初始蛇的长度是2
+	snake.x = COL / 2;//定义蛇头的位置
 	snake.y = ROW / 2;
-	body[0].x = COL / 2 - 1;
+	body[0].x = COL / 2 - 1;//蛇身体的位置，在蛇的后面
 	body[0].y = ROW / 2;
 	body[1].x = COL / 2 - 2;
 	body[1].y = ROW / 2;
-	face[snake.y][snake.x] = HEAD;
-	face[body[0].y][body[0].x] = BODY;
+	face[snake.y][snake.x] = HEAD;//记录位置为蛇头
+	face[body[0].y][body[0].x] = BODY;//记录位置为蛇身体
 	face[body[1].y][body[1].x] = BODY;
 }
 void RandFood()//随机函数
@@ -166,18 +176,18 @@ void RandFood()//随机函数
 	int x, y;
 	do
 	{
-		y = rand() % COL;
+		y = rand() % COL;//生成在行列以内的随机坐标
 		x = rand() % ROW;
-	} while (face[x][y] != KONG);
-	face[x][y] = FOOD;
-	CursorJump(2*y,x);//       *
+	} while (face[x][y] != KONG);//生成在位置为空的位置
+	face[x][y] = FOOD;//记录位置是食物位置
+	CursorJump(2*y,x);
 	printf("●");
 }
 void DrawSnake(int flag) //打印蛇 蛇的移动覆盖
 {
-	if (flag == 1)
+	if (flag == 1)//打印蛇
 	{
-		color(10);
+		ColorForInterface(10);//蛇的颜色为绿色
 		CursorJump(2*snake.x, snake.y);
 		printf("●");
 		for (int i = 0; i < snake.len; i++)
@@ -186,23 +196,23 @@ void DrawSnake(int flag) //打印蛇 蛇的移动覆盖
 			printf("□");
 		}
 	}
-	else
+	else//关于覆盖蛇
 	{
-		if (body[snake.len - 1].x != KONG)
+		if (body[snake.len - 1].x != KONG)//为了避免蛇的尾部的旁边是墙
 		{
-			CursorJump(2 * body[snake.len - 1].x, body[snake.len - 1].y);
+			CursorJump(2 * body[snake.len - 1].x, body[snake.len - 1].y);//跳转到蛇尾打印空格
 			printf(" ");
 		}
 	}
 }
-void JudgeFunc(int x, int y)
+void JudgeFunc(int x, int y)//判断游戏进度
 {
 	//若蛇头即将到达的位置是食物，则得分
 	if (face[snake.y + y][snake.x + x] == FOOD)
 	{
 		snake.len++; //蛇身加长
 		grade += 10; //更新当前得分
-		color(7); //颜色设置为白色
+		ColorForInterface(7); //颜色设置为白色
 		CursorJump(0, ROW);
 		printf("当前得分:%d", grade); //重新打印当前得分
 		RandFood(); //重新随机生成食物
@@ -211,12 +221,12 @@ void JudgeFunc(int x, int y)
 	{
 		Sleep(1000); //留给玩家反应时间
 		system("cls"); //清空屏幕
-		color(7); //颜色设置为白色
+		ColorForInterface(7); //颜色设置为白色
 		CursorJump(2 * (COL / 3), ROW / 2 - 3);
 		if (grade > max)
 		{
 			printf("恭喜你打破最高记录，最高记录更新为%d", grade);
-			WriteGrade();
+			UpDateGread();
 		}
 		else if (grade == max)
 		{
@@ -252,12 +262,12 @@ void JudgeFunc(int x, int y)
 		}
 	}
 }
-void MoveSnake(int x, int y)
+void MoveSnake(int x, int y)//蛇的移动
 {
 	DrawSnake(0); //先覆盖当前所显示的蛇
 	face[body[snake.len - 1].y][body[snake.len - 1].x] = KONG; //蛇移动后蛇尾重新标记为空
 	face[snake.y][snake.x] = BODY; //蛇移动后蛇头的位置变为蛇身
-	//蛇移动后各个蛇身位置坐标需要更新
+	//蛇移动后各个蛇身位置坐标需要更新，更改成其后一个位置的状态
 	for (int i = snake.len - 1; i > 0; i--)
 	{
 		body[i].x = body[i - 1].x;
@@ -271,13 +281,21 @@ void MoveSnake(int x, int y)
 	snake.y = snake.y + y;
 	DrawSnake(1); //打印移动后的蛇
 }
-void run(int x, int y)
+void PressTheKeyToGet(int x, int y)//获取键盘按键
 {
 	int t=0;
 	while (1)
 	{
-		if(t==0)
-		t = 10000;
+		if (t == 0)
+		{
+			if (grade <=50)
+				t = 10000;//表示获取按键的时间间隔
+			if (grade > 50 && grade <= 100)
+				t = 9000;
+			if (grade > 100)
+				t = 8000;
+		}
+			
 		while (--t)
 		{
 			if (kbhit() != 0) //若键盘被敲击，则退出循环
@@ -294,29 +312,30 @@ void run(int x, int y)
 		}
 	}
 }
-void Game()
+void Gamebodylogic()
 {
-	int n = RIGHT;
-	int tmp = 0;
-	goto first;
+	int n = RIGHT;//默认的蛇向右走
+	int MovingDirection = 0;//记录蛇移动的方向
+	goto first;//按键初始不同调整直接进first
 	while (1)
 	{
 		n = getch();
-		//键值的调整
+		//键值的调整，向上/下就不能直接向下/上了
+		//向左/右就不能直接向右/左了
 		switch (n)
 		{
 		case UP :
 		case DOWN:
-			if (tmp != LEFT && tmp != RIGHT)
+			if (MovingDirection != LEFT && MovingDirection != RIGHT)//向上/下就不能直接向下/上了
 			{
-				n = tmp;
+				n = MovingDirection;
 			}
 			break;
 		case RIGHT:
 		case LEFT:
-			if (tmp != UP && tmp != DOWN)
+			if (MovingDirection != UP && MovingDirection != DOWN)	//向左/右就不能直接向右/左了
 			{
-				n = tmp;
+				n = MovingDirection;
 			}
 		case SPACE:
 		case ESC:
@@ -324,34 +343,35 @@ void Game()
 		case 'R':
 			break;
 		default:
-			n = tmp;
+			n = MovingDirection;//其他按键均保持和上次运行方向一致
 			break;
 		}
 	first:
 		switch (n)
 		{
 		case UP:
-			run(0, -1);
-			tmp = UP;
+			//因为坐标系建在左上角，向上运动是纵坐标是减1，其他同理
+			PressTheKeyToGet(0, -1);
+			MovingDirection = UP;
 			break;
 		case DOWN:
-			run(0, 1);
-			tmp = DOWN;
+			PressTheKeyToGet(0, 1);
+			MovingDirection = DOWN;
 			break;
 		case LEFT:
-			run(-1,0);
-			tmp = LEFT;
+			PressTheKeyToGet(-1,0);
+			MovingDirection = LEFT;
 			break;
 		case RIGHT:
-			run(1, 0);
-			tmp = RIGHT;
+			PressTheKeyToGet(1, 0);
+			MovingDirection = RIGHT;
 			break;
 		case SPACE:
 			system("pause>nul");
 			break;
 		case ESC:
 			system("cls");
-			color(7);
+			ColorForInterface(7);
 			CursorJump(COL - 8, ROW / 2);
 			printf("  游戏结束  ");
 			CursorJump(COL - 8, ROW / 2 + 2);
@@ -370,8 +390,8 @@ int main()
 	max = 0, grade = 0;
 	char a;
 	system("title 贪吃蛇");
-	a = GameBegin();
-	color(7);
+	a = GameBegin();//游戏初始界面
+	ColorForInterface(7);
 	system("cls");
 	HideCursor();//光标隐藏
 	ReadMaxGrade();//读取max
@@ -380,7 +400,7 @@ int main()
 	srand((unsigned int)time(NULL)); //设置随机数生成起点
 	RandFood(); //随机生成食物
 	DrawSnake(1); //打印蛇
-	Game();
+	Gamebodylogic();
 	return 0;
 
 }
